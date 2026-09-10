7663878ac8cb85b4d452b6e92f7c6897e0aa9491
velam <velam@velam.com>
2026-09-10 12:15:18 +0200

    update aircraft et flyable
    ajout de membre de class

--- src/aircrafts/Aircraft.java
+++ src/aircrafts/Aircraft.java
@@ -2,6 +2,16 @@ package aircraft;
 
 public class Aircraft {
 
+  public enum Type{
+    JETPLANE,
+    HELICOPTER,
+    BALLON
+  }
+
+  public String name;
+  public float longitude;
+  public float latitude;
+  public float height;
   public Aircraft(){
     IO.println("Aircraft created");
   }
---
+++ src/aircrafts/Flyable.java
@@ -0,0 +1,5 @@
+package flyable;
+
+public class Flyable{
+
+}
